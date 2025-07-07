6202e6d62deaef8fd424275b1bd1bd08991fc6e0
Shen2806 <dmvuong12a2@gmail.com>
2025-07-07 17:34:12 +0700

    video 93

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,25 @@
 import { ArrowDownOutlined, ArrowRightOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Row, Col, Divider } from "antd";
-import { Link } from "react-router-dom";
+import { Button, Form, Input, Row, Col, Divider, message, notification } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { loginAPI } from "../services/api.service";
+import { useState } from "react";
 const LoginPage = () => {
+     const navigate = useNavigate();
     const [form] = Form.useForm();
-    const onFinish = (values) => {
-        console.log(">>> check values: ", values)
+    const [loading, setLoading] = useState(false);
+    const onFinish = async (values) => {
+        setLoading(true)
+        const res = await loginAPI(values.email, values.password)
+        if (res.data) {
+            message.success("Login successfully !")
+            navigate('/')
+        } else {
+            notification.error({
+                message: "Error Login",
+                description:JSON.stringify(res.message)
+            })
+        }
+        setLoading(false)
     }
     return (
         <Row justify={"center"} style={{ marginTop: "30px" }}>
@@ -44,7 +59,9 @@ const LoginPage = () => {
                                 alignItems:"center"
                                 
                             }}>
-                                <Button type="primary" onClick={()=>form.submit()}>
+                                <Button
+                                    loading={loading}
+                                    type="primary" onClick={() => form.submit()}>
                                     Login
                                 </Button>
                                 <Link to="/">Go to homepage <ArrowRightOutlined /></Link>
